bccbddb643a487f16bd270170151b52c2e7c03c6
apate107 <apate107@stevens.edu>
2019-10-08 23:12:19 -0400

    new data and outputs with updated merge conditions

--- merge_files.py
+++ merge_files.py
@@ -10,8 +10,9 @@ def read_file(fname, start):
     # Format and do a couple of checks on the lines because there are some lines that include the next sentence in it
     result = []
     for line in lines:
-        if line == '' or line.find(start) == -1:
+        if line == '' or line.replace("\\", "").find(start) == -1:
             continue
+        line = line.replace("\\", "")
         line = line[line.find(start):].strip()  # Some files had numbers beginning at each line
         if start == "He's" and line.find("She's") >= 0:
             line = line[0:line.find("She's")]
@@ -29,7 +30,7 @@ def main():
     with open("output\\male_all.txt", "w+") as m, open("output\\female_all.txt", "w+") as f:
         for fname in os.listdir(os.getcwd() + '\\data'):
             # Since male/he are within the words female/she, check for a female file otherwise default to male file
-            if fname.lower().find("female") >= 0 or fname.lower().find("she") >= 0:
+            if fname.lower().find("female") >= 0 or fname.lower().find("she") >= 0 or fname.lower().find("her") >= 0:
                 f.writelines(read_file('data\\' + fname, "She's"))
             else:
                 m.writelines(read_file('data\\' + fname, "He's"))
